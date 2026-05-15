503e2c52761070500ab814e22ce61e5b3d7d78db
jimm-volition <jui.mendoza@duocuc.cl>
2026-05-15 04:12:05 -0400

    se creo la conexion entre order y payment

--- microservicios/ms-order/src/main/java/com/marketplace/ms_order/service/OrderService.java
+++ microservicios/ms-order/src/main/java/com/marketplace/ms_order/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.marketplace.ms_order.service;
 
 import com.marketplace.ms_order.client.CartClient;
+import com.marketplace.ms_order.client.PaymentClient;
 import com.marketplace.ms_order.model.Order;
 import com.marketplace.ms_order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +15,10 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderService {
-
+    
     private final OrderRepository orderRepository;
     private final CartClient cartClient;
+    private final PaymentClient paymentClient;  
 
     public Order saveOrder(Order order) {
         order.setStatus("PENDING");
@@ -36,20 +38,29 @@ public class OrderService {
 
     public Order checkout(Long userId) {
         log.info("Getting cart for user {}", userId);
-        List<CartItemDto> cartItems =
-                cartClient.getCartByUser(userId);
+        List<CartItemDto> cartItems = cartClient.getCartByUser(userId);
         if(cartItems == null || cartItems.isEmpty()) {
             throw new RuntimeException("Cart is empty");
         }
         double total = 0;
         for(CartItemDto item : cartItems) {
-            total += item.getQuantity() * 10000;
+            total += item.getQuantity() * 10000; // Simulamos un precio fijo por producto para este ejemplo
         }
         Order order = new Order();
         order.setUserId(userId);
         order.setStatus("PENDING");
         order.setTotalAmount(total);
+        Order savedOrder = orderRepository.save(order);
+        log.info("Processing payment for order {}", savedOrder.getId());
+        String paymentStatus =
+                paymentClient.processPayment(savedOrder.getId());
+        log.info("Payment response: {}", paymentStatus);
+        if(paymentStatus.equals("APPROVED")) {
+            savedOrder.setStatus("CONFIRMED");
+        } else {
+            savedOrder.setStatus("REJECTED");
+        }
         log.info("Checkout completed for user {}", userId);
-        return orderRepository.save(order);
+        return orderRepository.save(savedOrder);
     }
 }
